696e21b9c85a69c4b5a04053c88ab1245513af34
Md Irteza Saquib <mdsaquib140802@gmail.com>
2025-06-12 12:41:32 +0530

    recommits

--- src/main/java/in/codingage/eatzy/controller/ContactController.java
+++ src/main/java/in/codingage/eatzy/controller/ContactController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/Contact")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 
 public class ContactController {
 
